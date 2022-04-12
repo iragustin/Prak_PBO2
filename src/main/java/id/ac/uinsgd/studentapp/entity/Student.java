d15f8cbed1b7cbaa2ae0f615a359e23c0fc1b87b
iragusin <hamkyagustin47@gmail.com>
2022-04-13 00:39:58 +0700

    tugas3

--- src/main/java/id/ac/uinsgd/studentapp/entity/Student.java
+++ src/main/java/id/ac/uinsgd/studentapp/entity/Student.java
@@ -14,19 +14,23 @@ public class Student {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @NotEmpty
+    @NotEmpty(message = "first name is required")
     @Column(name = "first_name")
     private String firstName;
 
-    @NotEmpty
+    @NotEmpty(message = "last name is required")
     @Column(name = "last_name")
     private String lastName;
 
-    @NotEmpty
+    @NotEmpty(message = "email is required")
     @Column(name = "email", unique = true)
     private String email;
 
-    @NotEmpty
+    @NotEmpty(message = "phone number is required")
     @Column(name = "phone_number", unique = true)
     private String phoneNumber;
+
+    @NotEmpty(message = "jurusan is required")
+    @Column(name = "jurusan", unique = true)
+    private String jurusan;
 }
